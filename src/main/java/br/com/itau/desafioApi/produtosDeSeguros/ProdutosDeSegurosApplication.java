768153ae496ee503d807f25ad74d46b162cba629
MariaFernanda2 <mariafernanda.samuniz@gmail.com>
2024-01-08 09:26:26 -0300

    configurando prometheus

--- src/main/java/br/com/itau/desafioApi/produtosDeSeguros/ProdutosDeSegurosApplication.java
+++ src/main/java/br/com/itau/desafioApi/produtosDeSeguros/ProdutosDeSegurosApplication.java
@@ -1,13 +1,21 @@
 package br.com.itau.desafioApi.produtosDeSeguros;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProdutosDeSegurosApplication {
 
+	@Bean
+	MeterRegistryCustomizer<PrometheusMeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", "ProdutosDeSegurosApplication");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosDeSegurosApplication.class, args);
 	}
-
 }
